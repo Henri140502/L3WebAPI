a82b74e605c889c5a20f54ad76c04ad0fc8f6d95
charonnet.henri <hcharonnet@gmail.com>
2025-03-19 13:31:41 +0000

    premier controlleur

---
+++ L3WebAPI.WebAPI/Controllers/GamesController.cs
@@ -0,0 +1,17 @@
+using L3WebAPI.Common.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L3WebAPI.WebAPI.Controllers;
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+[ApiController]
+public class GamesController : ControllerBase {
+    private readonly IGameService _gameService; 
+    public GamesController(IGameService gameService) {
+            _gameService = gameService;
+    }
+    [HttpGet("")] 
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<GameDTO>>> GetAllGames() { 
+        return Ok(await _gameService.GetAllGames());
+    }
+}
