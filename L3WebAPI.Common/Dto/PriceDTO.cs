1b2cd87e1317469e74e44cb842ca5da83ea4dccb
charonnet.henri <hcharonnet@gmail.com>
2025-03-19 14:20:14 +0000

    Price DAO to DTO

--- L3WebAPI.Common/Dto/PriceDTO.cs
+++ L3WebAPI.Common/Dto/PriceDTO.cs
@@ -1,3 +1,4 @@
+using L3WebAPI.Common.Dao;
 using L3WebAPI.Common.Request;
 
 namespace L3WebAPI.Common.Dto;
@@ -6,4 +7,16 @@ public class PriceDTO
 {
     public decimal valeur { get; set;}
     public Currency currency { get; set;}
+}
+
+public static class PriceDTOExtensions
+{
+    public static PriceDTO ToDTO(this PriceDAO priceDao)
+    {
+        return new PriceDTO
+        {
+            valeur = priceDao.valeur,
+            currency = (Currency)priceDao.currency
+        };
+    }
 }
