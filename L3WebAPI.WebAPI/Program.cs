a4af953d567f3c145f1c28633f0e6861a739c578
Elanis <elanis@hotmail.com>
2025-04-02 13:37:34 +0200

    Logs

--- L3WebAPI.WebAPI/Program.cs
+++ L3WebAPI.WebAPI/Program.cs
@@ -2,44 +2,61 @@ using L3WebApi.Business.Implementations;
 using L3WebApi.Business.Interfaces;
 using L3WebAPI.DataAccess.Implementations;
 using L3WebAPI.DataAccess.Interfaces;
+using NLog;
+using NLog.Web;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using System.Text.Json.Serialization;
 
 public class Program {
 
 	public static void Main(string[] args) {
-		var builder = WebApplication.CreateBuilder(args);
+		var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+		logger.Debug("Starting up app ...");
 
-		// Add services to the container.
-		builder.Services.AddTransient<IGamesDataAccess, GamesDataAccess>();
-		builder.Services.AddTransient<IGamesService, GamesService>();
+		try {
+			var builder = WebApplication.CreateBuilder(args);
 
-		builder.Services.AddControllers().AddJsonOptions(options => {
-			options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-		});
-		builder.Services.AddOpenApi();
+			// Add services to the container.
+			builder.Services.AddTransient<IGamesDataAccess, GamesDataAccess>();
+			builder.Services.AddTransient<IGamesService, GamesService>();
 
-		var app = builder.Build();
+			builder.Services.AddControllers().AddJsonOptions(options => {
+				options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+			});
+			builder.Services.AddOpenApi();
 
-		// Configure the HTTP request pipeline.
-		if (app.Environment.IsDevelopment()) {
-			app.MapOpenApi();
+			builder.Logging.ClearProviders();
+			builder.Host.UseNLog();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment()) {
+				app.MapOpenApi();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+			app.MapControllers();
+
+			app.UseSwaggerUI(options => {
+				options.ConfigObject.Urls = [new UrlDescriptor {
+					Name = "L3 Web API",
+					Url = "/openapi/v1.json"
+				}];
+			});
+
+			app.Run();
+
+		} catch (Exception exception) {
+			// NLog: catch setup errors
+			logger.Error(exception, "Stopped program because of exception");
+			throw;
+		} finally {
+			// Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+			NLog.LogManager.Shutdown();
 		}
-
-		app.UseHttpsRedirection();
-
-		app.UseAuthorization();
-
-		app.MapControllers();
-
-		app.UseSwaggerUI(options => {
-			options.ConfigObject.Urls = [new UrlDescriptor {
-		Name = "L3 Web API",
-		Url = "/openapi/v1.json"
-	}];
-		});
-
-		app.Run();
-
 	}
 }
