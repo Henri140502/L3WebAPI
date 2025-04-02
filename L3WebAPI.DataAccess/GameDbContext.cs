149da27a8731997efa90db67a82b2422184f626a
Elanis <elanis@hotmail.com>
2025-04-02 15:38:43 +0200

    EfCore pt.1

---
+++ L3WebAPI.DataAccess/GameDbContext.cs
@@ -0,0 +1,46 @@
+﻿using L3WebAPI.Common.Dao;
+using Microsoft.EntityFrameworkCore;
+
+namespace L3WebAPI.DataAccess {
+	public class GameDbContext : DbContext {
+		public DbSet<GameDAO> Games { get; set; }
+		// public DbSet<UserDAO> Users { get; set; }
+
+		public GameDbContext(DbContextOptions<GameDbContext> options)
+			: base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder builder) {
+			var gameDaoBuilder = builder.Entity<GameDAO>();
+
+			gameDaoBuilder.Property(x => x.AppId)
+				.HasColumnName("app_id")
+				.HasColumnType("uuid");
+
+			// Primary Key
+			gameDaoBuilder.HasKey(x => x.AppId);
+
+			gameDaoBuilder.Property(x => x.Name)
+				.HasColumnName("name")
+				.HasMaxLength(1000)
+				.IsUnicode(true);
+
+			// gameDaoBuilder.HasIndex(x => x.Name).IsUnique();
+
+			var priceDaoBuilder = builder.Entity<PriceDAO>();
+
+			priceDaoBuilder.Property(x => x.Valeur)
+				.HasColumnName("valeur")
+				.HasPrecision(5, 2);
+
+			priceDaoBuilder.Property(x => x.Currency);
+
+			// Foreign Key
+			//gameDaoBuilder.HasMany(x => x.Prices);
+			//gameDaoBuilder.HasMany(x => x.Prices).WithOne(x => x.Game);
+			gameDaoBuilder.OwnsMany(x => x.Prices);
+
+
+
+		}
+	}
+}
